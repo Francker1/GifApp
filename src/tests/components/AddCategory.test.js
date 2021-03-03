d232504176748d4355ffa80c67da0f2ab6a2142c
Ítalo Franco <italo.ravina@gmail.com>
2021-03-03 01:19:07 +0100

    test: finish with AddCategory component tests

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import "@testing-library/jest-dom";
 import { shallow } from "enzyme";
 import { AddCategory } from "../../components/AddCategory";
 
 describe("test in AddCategory component", () => {
 
-    const setCategories = () => {};
-    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+    beforeEach(() => {
+
+        wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    });
 
     test("shoul render AddCategory correctly", () => {
     
@@ -24,5 +30,37 @@ describe("test in AddCategory component", () => {
         });
 
         expect(wrapper.find("p").text().trim()).toBe(value);
-    })
+    });
+
+    test("Don't post info submit with inputValue empty", () => {
+
+        wrapper.find("form").simulate("submit", { preventDefault(){} });
+
+        expect( setCategories ).not.toHaveBeenCalled();
+    });
+
+    test("Should call setCategories and clean input box", () => {
+
+        const value = "Homer";
+
+        //1. simular el inputChange
+        wrapper.find("input").simulate("change", { target: { value } });
+
+        //2. simular el submit
+        wrapper.find("form").simulate("submit", { preventDefault() {} });
+
+        //3. setCategories debe haberse llamado
+        expect( setCategories ).toHaveBeenCalled();
+        
+        //if I want check times the function has been called:
+        expect( setCategories ).toHaveBeenCalledTimes(1);
+
+        //if I check the function has been called with function (because setCategories expect a function)
+        expect( setCategories ).toHaveBeenCalledWith( expect.any(Function) )
+
+        //4. el valor del input debe ser vacío
+        expect( wrapper.find("input").prop("value") ).toBe("");
+
+
+    });
 });
